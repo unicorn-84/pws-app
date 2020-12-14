bd4fedb069cfb6854d0c1bc4f147688da7d6ca89
unicorn-84 <unicorn-84@ya.ru>
2020-12-14 21:17:25 +0300

    added material-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import ThemeProvider from '@material-ui/styles/ThemeProvider';
+
+import theme from './theme';
+import AppContainer from './App.styles';
 
 export default () => (
-  <></>
+  <React.StrictMode>
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <AppContainer>
+        dev.kapitonov
+      </AppContainer>
+    </ThemeProvider>
+  </React.StrictMode>
 );
